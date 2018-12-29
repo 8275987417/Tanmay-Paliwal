4131fa1b6ca22e9e024cd7a55152b8f7b8f9e8d9
Tanmay Nityanand Paliwal <45824743+Tanmay8275987417@users.noreply.github.com>
2018-12-29 13:28:46 +0530

    add project

---
+++ project1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Array();
+for (x = 0; x < 6; x++)
+    {
+    for (y = 0; y < 6; y++)
+    {
+        for (z = 0; z < 6; z++)
+        {
+            [x][y][z] = x * y * z;
+        }
+    }
+    }
+getch();
+}
